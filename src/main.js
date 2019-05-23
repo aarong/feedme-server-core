515bf82c44111e94b50522934ad207b26a408885
Aaron G <aarongoertzen@gmail.com>
2019-05-23 08:01:33 -0400

    Linting

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/**
+ * Outward-facing server factory function.
+ *
+ * Takes a transport from outside, wraps it, and injects it into the server.
+ */
+export default function feedmeServer() {
+  console.log("Feedme server factory function.");
+}
